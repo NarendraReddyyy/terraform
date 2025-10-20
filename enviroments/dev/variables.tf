b227f07736925e1a030f9c90df1f1804dd43a0db
narendra <narendrareddy@gmail.com>
2025-10-20 17:12:50 -0500

    initial commit

---
+++ enviroments/dev/variables.tf
@@ -0,0 +1,14 @@
+# variable "resource_groups" {
+#   type = map(object({
+#     name     = string
+#     location = string
+#   }))
+# }
+variable "sijarvis_resource_groups_ukso" {
+  type = map(object({
+    name = string
+    location = string
+    managed_by = optional(string, null)
+  }))
+  description = "Map for resource groups"
+}
