27f0b16059dbd0e438237507241fa1aea1294189
narendra <narendrareddy@gmail.com>
2025-10-20 17:22:00 -0500

    initial commit

--- enviroments/dev/main.tf
+++
@@ -1,6 +0,0 @@
-module "sijarvis_resource_group_ukso" {
-  source = "../../modules/resource-group"
-
-  # resource_groups = var.resource_groups
-  resource_groups = var.sijarvis_resource_groups_ukso
-}
